d55965d0e15e11f4c3301003f707d7aa3e547181
Bandashah <58322042+Bandashah@users.noreply.github.com>
2019-12-04 11:34:27 +0500

    Create StringEncryptionWithRandomKeyGeneration.vb

---
+++ StringEncryptionWithRandomKeyGeneration.vb
@@ -0,0 +1,76 @@
+Module Module1
+
+    Sub Main()
+        Dim key(127) As String
+        Dim pt As String
+        Dim estring, dstring, dchar As String
+        Dim choice, x, upperbound, lowerbound As Integer
+        Dim character As Char
+        estring = ""
+        dstring = ""
+        pt = ""
+        lowerbound = 0
+        upperbound = 127
+        For c = 0 To 127
+            x = Int((upperbound - lowerbound + 1) * Rnd() + lowerbound)
+            key(c) = ChrW(x)
+        Next
+        While choice <> 3
+            Console.Write("press 1 to encrypt, 2 decrypt and 3 to exit :")
+            choice = Console.ReadLine()
+            Select Case choice
+                Case 1 : Console.Write("Enter the plain text to encrypt :")
+                    pt = Console.ReadLine()
+                    estring = EncryptString(key, pt)
+                    Console.Write("the encrypted text :" & estring)
+                Case 2 : For n = 1 To Len(estring)
+                        character = Mid(estring, n, 1)
+                        dchar = decrypt(key, character)
+                        dstring = dstring & dchar
+                    Next
+                    Console.WriteLine("the decrypted string : " & dstring)
+
+                Case 3
+            End Select
+            Console.WriteLine()
+        End While
+
+
+        Console.ReadKey()
+    End Sub
+    Function EncryptString(ByVal LookUp As Array, ByVal PlainText As String) As String
+
+        Dim OldChar, NewChar As Char
+        Dim OldCharValue As Integer
+        Dim OutString As String
+
+        'first initialise the return string
+
+        OutString = "" 'initialise the return string
+        'loop through PlainText to produce OutString
+        For n = 1 To Len(PlainText) 'from first to last character
+            OldChar = Mid(PlainText, n, 1) 'get next character
+            OldCharValue = Asc(OldChar) 'find the ASCII value
+            NewChar = LookUp(OldCharValue) 'look up substitute character
+            OutString = OutString & NewChar ' concatenate to OutString
+        Next
+        Return OutString 'EncryptString © OutString
+    End Function
+
+    Function decrypt(ByVal lookup As Array, ByVal chipherchar As Char) As Char
+        Dim found As Boolean
+        Dim index As Integer
+        Dim originalchar As Char
+        index = 1
+        found = False
+        While found = False
+            If lookup(index) = chipherchar Then
+                found = True
+            Else
+                index = index + 1
+            End If
+        End While
+        originalchar = Chr(index)
+        Return originalchar
+    End Function
+End Module
