5a980c02c3334e6b89238e5a24fd1a6138ae573e
Bandashah <58322042+Bandashah@users.noreply.github.com>
2019-12-19 14:58:50 +0500

    Create BubbleSort_Enhanced.vb

---
+++ BubbleSort_Enhanced.vb
@@ -0,0 +1,37 @@
+'Bubble Sort Visual Basic Code
+
+Module Module1
+
+    Sub main()
+        Dim height(5) As Integer
+        Dim swapped As Boolean
+        Dim temp As Integer
+        Dim maxsize As Integer
+        maxsize = 5
+
+        For count = 1 To 5
+            height(count) = Console.ReadLine()
+        Next
+        'sort the elements
+        Do
+            swapped = False
+            For count = 1 To maxsize - 1
+
+                If height(count + 1) < height(count) Then
+                    temp = height(count)
+                    height(count) = height(count + 1)
+                    height(count + 1) = temp
+                    swapped = True
+                End If
+            Next
+            maxsize = maxsize - 1
+        Loop Until swapped = False
+
+        'Print out the elements
+        For count = 1 To maxsize
+            Console.WriteLine(count & ": " & height(count))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
