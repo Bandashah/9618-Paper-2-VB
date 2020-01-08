6b5240e759bea77fd3a12171dcb070c68f1e2103
Bandashah <58322042+Bandashah@users.noreply.github.com>
2020-01-08 17:42:44 +0500

    Create Bubblesort_Everything.vb

---
+++ Bubblesort_Everything.vb
@@ -0,0 +1,33 @@
+Sub main()
+        Dim height() = {45, 67, 23, 45, 21, 78, 90, 11, 21}
+        Dim swapped As Boolean
+        Dim temp As Integer
+        Dim maxsize As Integer
+        maxsize = 8
+        Dim counter As Integer
+        swapped = True
+        While swapped <> False ' Do
+            swapped = False
+            'For x = 0 To 8
+            For count = 0 To 7 'maxsize - 1
+
+                If height(count + 1) < height(count) Then
+                    temp = height(count)
+                    height(count) = height(count + 1)
+                    height(count + 1) = temp
+                    ' swapped = True
+                End If
+                counter = counter + 1
+            Next
+            ' maxsize = maxsize - 1
+        End While 'Loop Until swapped = False
+        ' Next
+        'Print out the elements
+        For count = 0 To 8
+            Console.WriteLine(count & ": " & height(count))
+        Next
+        Console.WriteLine("the algo analysis : " & counter)
+        Console.ReadKey()
+    End Sub
+
+End Module
