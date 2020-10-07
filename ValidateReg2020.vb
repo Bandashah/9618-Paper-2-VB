7017e8c2907d45cd6ad00e179bc802d74a2037eb
Bandashah <58322042+Bandashah@users.noreply.github.com>
2020-10-07 17:52:07 +0500

    Create ValidateReg2020.vb

---
+++ ValidateReg2020.vb
@@ -0,0 +1,29 @@
+Module Module1
+
+    Sub Main()
+        Console.WriteLine(ValidateRegistration("ACV45VDFF"))
+
+        Console.ReadKey()
+
+    End Sub
+
+    Function ValidateRegistration(ByVal Registration As String) As Boolean
+        Dim validate As Boolean
+        Dim nextchar As Char
+        validate = True
+        If Len(Registration) <> 6 And Len(Registration) <> 9 Then
+            validate = False
+        End If
+        For count = 1 To Len(Registration)
+            nextchar = Mid(Registration, count, 1)
+            If nextchar <> UCase(nextchar) Then validate = False
+        Next
+        For count = 4 To 5
+            nextchar = Mid(Registration, count, 1)
+            If nextchar < "0" Or nextchar > "9" Then validate = False
+        Next
+
+
+        Return validate
+    End Function
+End Module
